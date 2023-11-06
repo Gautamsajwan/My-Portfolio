c5c561ba3605af8527c0bfbefa0fb89add6624cc
gautam <gautamsajwan8126@gmail.com>
2023-11-06 11:47:54 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [{
+            protocol: 'https',
+            hostname: 'images6.alphacoders.com',
+            },
+        ]
+    },
+}
 
 module.exports = nextConfig
