973f0dd530c0c0a4dac07e707c9def0d5f9ca5d8
gautam <gautamsajwan8126@gmail.com>
2023-11-10 11:51:53 +0530

    optimized responsiveness

--- components/ExpCard.tsx
+++ components/ExpCard.tsx
@@ -13,7 +13,7 @@ export default function ExpCard({ logo, eventName, org, duration, work }: Props)
     <article className="phone:w-[300px] w-[350px] lg:w-[420px] xl:w-[480px] h-full px-5 py-4 snap-center cursor-pointer flex flex-col items-center flex-shrink-0 rounded-2xl bg-[#2a2c2b] overflow-hidden z-20">
       <img className="h-24 w-24 object-cover rounded-full bg-gray-500" src={logo} alt="brand logo" />
 
-      <div className="h-[78%] mt-1 py-2 outline lg:px-3 flex flex-col items-center">
+      <div className="h-[78%] mt-1 py-2 lg:px-3 flex flex-col items-center">
         <div className="h-[150px] w-full flex flex-col items-center">
           <h2 className="text-2xl text-center font-semibold">{eventName}</h2>
           <p className="mt-2 p-2 h-[65px] w-full text-base grid place-items-center font-bold text-center bg-black/40">{org}</p>
